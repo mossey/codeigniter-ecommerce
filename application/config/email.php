c5ffc838f3510895b602dac491a8c0523c7b19de
Sergiu Finciuc <ser.finciuc@gmail.com>
2015-08-25 17:40:52 +0300

    version adapted to host

--- application/config/email.php
+++ application/config/email.php
@@ -1,13 +1,1 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['mailtype'] = 'html';
-$config['charset'] = 'utf-8';
-
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'mail.freshmarket.md';
-$config['smtp_port'] = 25;
-$config['smtp_user'] = 'info@freshmarket.md';
-$config['smtp_pass'] = 'amplica123';
-$config['smtp_timeout'] = '4';
-$config['crlf'] = '\n';
-$config['newline'] = '\r\n';
